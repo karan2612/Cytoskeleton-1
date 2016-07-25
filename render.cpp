7723b0ec36351b942cb2546611ea33ad5e516cda
Tony Qian <tq2115@columbia.edu>
2016-07-25 15:54:34 +0200

    updated to Friday's render.C

--- render.cpp
+++ render.cpp
@@ -1,5 +1,6 @@
 #include "gr.h"
 #include "gr3.h"
+
 #include "stdio.h"
 
 #include <iostream>
@@ -8,40 +9,59 @@
 
 using namespace std;
 
-int main(void) {
-  
-  ifstream infile("positions.txt", ios::in);
-  vector<double> x;
+void read(){
+}
 
-  double num  = 0.0;
-  while ( infile >> num ) {
-    x.push_back(num);
+int main(void) {
+
+  double num;
+  vector<double> x(1024);
+
+  ifstream infile("newPositions.txt", ios::in);
+  string str;
+
+  int T,X;
+  infile >> T;
+  infile >> X;
+  X = 3*X;
+  float in[T][X];
+  for(int t=0; t<T; t++) {
+    for(int x=0; x<X; x++) {
+      infile >> num;
+      in[t][x] = num;
+    }
   }
 
-  float positions[] = {-1, 0, 0, 1, 0, 0};
+  float colors[] = {1,0,0, 1,1,1, 1,0,0};
+  float radii[] = {0.25, 0.25, 0.25};
+  float directions[] = {1, 0, 0};
 
-  for(size_t i =0; i < x.size(); i++) {
 
-    float colors[] = {1, 1, 1, 1, 0, 0};
-    float radii[] = {0.25, 0.25};
-    float directions[] = {1, 0, 0};
-    float lengths[] = {positions[3] - positions[0]};
-    float cyl_radius[] = {0.1};
+  float cyl_colors[] = {1,1,1, 1,1,1};
+  float cyl_radius[] = {0.1, 0.1};
+  float cyl_dir[] = {1,0,0, 1,0,0};
 
-    //    cout << x[i] << endl;
 
-    positions[3] = x[i];
+  float* positions;
+  for(int t=0; t<T; t++) {
+    float lengths[] = {in[t][3] - in[t][0],
+		       in[t][6] - in[t][3]};
+    //this is savage. what if non adjacent?
 
+    positions = in[t];
+    
     gr3_clear();
-    gr3_drawspheremesh(2, positions, colors, radii);
-    gr3_drawcylindermesh(1, positions, directions, colors, cyl_radius, lengths);
+    gr3_drawspheremesh(3, positions, colors, radii);
+    gr3_drawcylindermesh(2, positions, cyl_dir,
+			 cyl_colors, cyl_radius, lengths);
 
     gr_clearws();
     gr_setviewport(0, 1, 0, 1);
     gr3_drawimage(0, 1, 0, 1, 500, 500, GR3_DRAWABLE_GKS);
     gr_updatews();    
+
   }
+
   getc(stdin);
-  //gr3_export("test.html", 1000, 1000);                                                                                 
   return 0;
 }
